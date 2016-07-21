455b1175e9fc4d59def775057222ec320e0ec7b1
yoguido77 <yoguido77@gmail.com>
2016-07-21 17:59:22 -0500

    added about screen

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Guido on 7/21/16.
+//  Copyright © 2016 Guido J. Medina. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+}
