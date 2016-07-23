e85fe8bc636a052a92d2c23790a03be4221c2e29
yoguido77 <yoguido77@gmail.com>
2016-07-23 11:57:48 -0500

    added crossfade animation

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import QuartzCore
+
 
 class ViewController: UIViewController {
     var currentValue = 0
@@ -135,6 +137,12 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
     }
 }
 
